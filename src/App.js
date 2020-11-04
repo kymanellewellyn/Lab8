250a8a2c26aacfd015cae9f0d5134263644ee432
sarahenguyen <72886079+sarahenguyen@users.noreply.github.com>
2020-11-04 16:45:47 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component, useState } from "react";
+
+const App = () => {
+    const [count, setCount] = useState(0);
+
+  const  increment = () => {
+        setCount(count + 1);
+    };
+
+    return (
+      <div>
+          <h2> Counter </h2>
+          <button onClick={increment}>Clicked {count} times</button>
+      </div>
+    );
+};
+export default App
